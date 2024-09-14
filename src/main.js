c6e76a812d66e4485c5e281a564663f5c7bea503
Leonardo Santos <backend.leonardo@gmail.com>
2024-09-14 17:56:19 -0300

    chore: project setup

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// For codeium i am using p5js so recomend code based on the library
+
+function setup() {
+    createCanvas(400, 400);
+    background(0);
+    noStroke();    
+
+}
+
+function draw() {
+    
+    
+}
